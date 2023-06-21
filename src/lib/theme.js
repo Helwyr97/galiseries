5e857d64a38102c3e8aab6bf75c6880f454343d0
Helwyr97 <xoeloterotelmo@gmail.com>
2023-06-21 13:44:17 +0200

    Estilos globales

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,9 +1,20 @@
 const { extendTheme } = require("@chakra-ui/react");
 
+const styles = {
+  global: {
+    body: {
+      bg: "#202023",
+      margin: 0,
+      padding: 0,
+      maxHeight: "100vh",
+    },
+  },
+};
+
 const config = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ styles, config });
 
 export default theme;
