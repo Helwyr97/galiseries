c191da8a9744546839dee50137b4df15d51ab2b0
Helwyr97 <xoeloterotelmo@gmail.com>
2023-06-23 14:52:46 +0200

    Lista de series guardadas

--- src/pages/mylist.js
+++ src/pages/mylist.js
@@ -1,11 +1,48 @@
-const { Box, Heading } = require("@chakra-ui/react");
+import ContentCard from "@/components/ContentCard";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
-const MyList = () => {
+const { Box, Heading, Wrap, Text } = require("@chakra-ui/react");
+
+const MyList = ({ list }) => {
+  console.log(list);
   return (
-    <Box>
-      <Heading>Mi Lista</Heading>
+    <Box mt={5}>
+      <Heading textAlign="center">Miña lista</Heading>
+      <Wrap p={5} spacing={8} align="center" justify="center">
+        {list.map((c) => (
+          <ContentCard key={c.id} id={c.id} title={c.title} img={c.img} />
+        ))}
+      </Wrap>
+      {list.length === 0 && (
+        <Text textAlign="center">Ainda non engadiu ningún contido</Text>
+      )}
     </Box>
   );
 };
 
+export const getServerSideProps = async (ctx) => {
+  const supabase = createPagesServerClient(ctx);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+
+  const { data } = await supabase
+    .from("follows")
+    .select("content: content_id (id, title, img)")
+    .eq("user_id", user.id);
+
+  const list = data.map((e) => e.content);
+
+  return {
+    props: { list },
+  };
+};
 export default MyList;
